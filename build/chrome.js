e9477e0c14f59e5d0ebac852415cccd27a6fad8c
Max Fechner <max.fechner@gmail.com>
2016-12-11 23:05:53 +0100

    ...

--- build/chrome.js
+++ build/chrome.js
@@ -1,5 +1,18 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.tabs.create({
-		url: 'csp.html'
-	});
-});
+// chrome.app.runtime.onLaunched.addListener(function() {
+// 	chrome.tabs.create({
+// 		url: 'csp.html'
+// 	});
+// });
+
+
+// chrome.app.runtime.onLaunched.addListener(function() {
+// 	chrome.app.window.create('index.html', {
+// 		'innerBounds': {
+// 			'width': 400,
+// 			'height': 700,
+// 			'minWidth': 300,
+// 			'maxWidth': 500,
+// 		},
+// 		'frame': 'none'
+// 	});
+// });
